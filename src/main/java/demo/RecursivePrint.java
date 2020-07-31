924c69d814243e5c33909530a6a3fb59ec29cf3d
Vinoth Venugopal <v.vinodhan@gmail.com>
2020-07-31 14:28:30 +0530

    initial commit

---
+++ src/main/java/demo/RecursivePrint.java
@@ -0,0 +1,28 @@
+package demo;
+
+import java.util.Arrays;
+
+public class RecursivePrint {
+
+	public static void main(String[] args) {
+
+		String str = "hello";
+		recPrint(str);
+	}
+
+	static void recPrint(String s)
+	{
+		while(s.length() > 0)
+		{
+			System.out.println(s.charAt(0));
+			s = s.substring(s.length()-(s.length()-1));
+		//	System.out.println(s.length());
+			if(s.length()>0)
+			{
+				recPrint(s);
+				break;
+			}
+		}
+	}
+
+}
